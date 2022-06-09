63815684227c3a5fe151310a4b864c5f3b5e953c
Ryan Smith <ryan.l.smith142@gmail.com>
2022-06-09 15:56:05 -0500

    Add heroku

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-    console.log(__dirname);
+    console.log("hey")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
